e30eddd4b677d1851c0a65af37a4a5b1fd7381f1
Bao Jun <baojun.cz@gmail.com>
2019-06-11 15:57:28 +0800

    chore: remove unused code

--- lib/base/utils/searchTransaction.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:typed_data';
-
-import 'package:ckb_sdk/ckb-types/item/transaction.dart';
-
-Future<List<Transaction>> searchTransaction(Uint8List privateKey) async {
-  return List<Transaction>(0);
-}
